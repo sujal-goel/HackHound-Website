0a26ebdf1e4836ee7f9069328ba8a750990d996a
Dhruv Kalra <kalradhruv2003@gmail.com>
2023-11-17 13:01:06 +0530

    smtp service activated

--- Js/contact-script.js
+++ Js/contact-script.js
@@ -1,14 +1,18 @@
-function sendEmail(){
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "",
-        Password : "",
-        To : '',
-        From : document.getElementById("email").value,
-        Subject : "New Contact Form Enquiry",
-        Body : "Name" + document.getElementById("name").value + "<br> Email: " + document.getElementById("email").value + 
-        "<br> Contact No.: " + document.getElementById("phone").value + "<br> Message: " + document.getElementById("message").value
-    }).then(
-      message => alert("Message Sent Successfully")
-    );
+function sendEmail() {
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var phone = document.getElementById("phone").value;
+  var message = document.getElementById("message").value;
+  var body = "Name : " + name + "<br> Email : " + email + "<br> Contact Number : " + phone + "<br> Message : " + message;
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "hackhound.srm@gmail.com",
+    Password: "3EB18B8C3F0BEA991DA25275482DE40B3C42",
+    To: 'hackhound.srm@gmail.com',
+    From: "hackhound.srm@gmail.com",
+    Subject: "New Contact Form Enquiry",
+    Body: body
+  }).then(
+    message => alert("Message Sent Successfully")
+  );
 }
